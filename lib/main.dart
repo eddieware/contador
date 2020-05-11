d37341321479383fbffeac227d85065c6493f1cf
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-05-11 15:08:09 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(context){ //context contiene informacion
+  return MaterialApp(
+    home: 
+    Center(child: Text('Hola Mundo'),
+    ),
+    
+  );
+
+  }
+
+}
